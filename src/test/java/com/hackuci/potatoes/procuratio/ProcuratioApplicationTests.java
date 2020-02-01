4993b74ea92b69b957776635cc68cf7987e7503e
Jimmy Dang <immmjimmy@gmail.com>
2020-01-31 19:26:05 -0800

    initial commit

---
+++ src/test/java/com/hackuci/potatoes/procuratio/ProcuratioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackuci.potatoes.procuratio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcuratioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
