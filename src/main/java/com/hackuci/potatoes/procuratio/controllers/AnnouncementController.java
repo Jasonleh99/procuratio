9e8e64637a6ae07612f688327abd543ed6af5870
Jason Le <36774043+Jasonleh99@users.noreply.github.com>
2020-02-01 13:33:47 -0800

    Implemented announcement controller

--- src/main/java/com/hackuci/potatoes/procuratio/controllers/AnnouncementController.java
+++ src/main/java/com/hackuci/potatoes/procuratio/controllers/AnnouncementController.java
@@ -1,19 +1,26 @@
 package com.hackuci.potatoes.procuratio.controllers;
 
 import java.util.Collection;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hackuci.potatoes.procuratio.models.Announcement;
+import com.hackuci.potatoes.procuratio.models.Teacher;
 import com.hackuci.potatoes.procuratio.repositories.AnnouncementRepository;
+import com.hackuci.potatoes.procuratio.repositories.TeacherRepository;
 
 @RestController
 @RequestMapping("/api/announcements")
 public class AnnouncementController {
 //need to make repository
 	private AnnouncementRepository announcementRepository;
+	private TeacherRepository teacherRepository;
 
 public AnnouncementController(AnnouncementRepository announcementRepository) {
 	super();
@@ -21,9 +28,12 @@ public AnnouncementController(AnnouncementRepository announcementRepository) {
 }
 	
 
-	@GetMapping("/")
-	Collection<Announcement> getannouncements() {
-		return announcementRepository.findAll();
+	@GetMapping("/{teacherid}")
+	ResponseEntity<?> getannouncements(@PathVariable Long teacherid) {
+		Optional<Teacher> teacher = teacherRepository.findById(teacherid);
+		return teacher.map(response -> ResponseEntity.ok().body(
+				announcementRepository.findByTeacher(response)))
+			.orElse(ResponseEntity<>(HttpStatus.NOT_FOUND));
 		//going to get all the announcements from the repository
 	}
 	
--- src/main/java/com/hackuci/potatoes/procuratio/repositories/AnnouncementRepository.java
+++ src/main/java/com/hackuci/potatoes/procuratio/repositories/AnnouncementRepository.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hackuci.potatoes.procuratio.models.Announcement;
+import com.hackuci.potatoes.procuratio.models.Teacher;
 
 public interface AnnouncementRepository extends JpaRepository<Announcement, Long>{
 	List<Announcement> findByTitle(String title);
+	List<Announcement> findByTeacher(Teacher teacher);
 }
