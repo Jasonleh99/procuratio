808bd305cd92e6cf063717dc10dd3b8cb181db55
Jason Le <36774043+Jasonleh99@users.noreply.github.com>
2020-02-01 14:07:49 -0800

    oops

--- src/main/java/com/hackuci/potatoes/procuratio/repositories/DocumentRepository.java
+++ src/main/java/com/hackuci/potatoes/procuratio/repositories/DocumentRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hackuci.potatoes.procuratio.models.Document;
+import com.hackuci.potatoes.procuratio.models.Teacher;
 
 public interface DocumentRepository extends JpaRepository<Document, Long>{
 	List<Document> findByTitle(String title);
