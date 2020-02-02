7cfe7077dc025fe3256577fbb2749ec3ca5b7fcf
Gina Le <GInaLe1@dhcp-v095-056.mobile.uci.edu>
2020-02-02 08:27:11 -0800

    Added a lot of data

---
+++ src/main/resources/GinaData.sql
@@ -0,0 +1,280 @@
+-- id, login, name, password, usertype
+insert into user values (1, 'Snig', 'snig123', 'qwert', 0);
+insert into user values (2, 'Easton', 'easton123', 'qwert', 0);
+insert into user values (3, 'Doo', 'doo123', 'qwert', 0);
+insert into user values (4, 'Lincoln', 'lincoln123', 'qwert', 0);
+insert into user values (5, 'Andrew', 'andrew123', 'qwert', 0);
+insert into user values (6, 'Phine', 'phine123', 'qwert', 0);
+insert into user values (7, 'Josephine', 'josephine123', 'qwert', 0);
+insert into user values (8, 'Priscilla', 'priscilla123', 'qwert', 0);
+insert into user values (9, 'Nikki', 'nikki123', 'qwert', 0);
+insert into user values (10, 'Michael', 'michael123', 'qwert', 0);
+insert into user values (11, 'Steven', 'steven123', 'qwert', 0);
+insert into user values (12, 'Stephen', 'stephen123', 'qwert', 0);
+insert into user values (13, 'Susan', 'susan123', 'qwert', 0);
+insert into user values (14, 'Natalie', 'natalie123', 'qwert', 0);
+insert into user values (15, 'Kelly', 'kelly123', 'qwert', 0);
+insert into user values (16, 'Kennith', 'kennith123', 'qwert', 0);
+insert into user values (17, 'Tina', 'tina123', 'qwert', 0);
+insert into user values (18, 'Nhat', 'nhat123', 'qwert', 0);
+insert into user values (19, 'Kenny', 'kenny123', 'qwert', 0);
+insert into user values (20, 'Kayla', 'kayla123', 'qwert', 0);
+insert into user values (21, 'Dennis', 'dennis123', 'qwert', 0);
+insert into user values (22, 'Hubert', 'hubert123', 'qwert', 0);
+insert into user values (23, 'Barbie', 'barbie123', 'qwert', 0);
+insert into user values (24, 'Kyla', 'kyla123', 'qwert', 0);
+insert into user values (25, 'Ashley', 'ashley123', 'qwert', 0);
+insert into user values (26, 'Jessica', 'jessica123', 'qwert', 0);
+insert into user values (27, 'Tanmay', 'tanmay123', 'qwert', 0);
+insert into user values (28, 'Crystal', 'crystal123', 'qwert', 0);
+insert into user values (29, 'Romeo', 'romeo123', 'qwert', 0);
+insert into user values (30, 'Juliet', 'juliet123', 'qwert', 0);
+insert into user values (31, 'Kendra', 'kendra123', 'qwert', 0);
+insert into user values (32, 'Mike', 'mike123', 'qwert', 0);
+insert into user values (33, 'Sarah', 'sarah123', 'qwert', 0);
+insert into user values (34, 'Dani', 'dani123', 'qwert', 0);
+insert into user values (35, 'Daniella', 'daniella123', 'qwert', 0);
+insert into user values (36, 'Genie', 'genie123', 'qwert', 0);
+insert into user values (37, 'Hillary', 'hillar123', 'qwert', 0);
+insert into user values (38, 'Darlene', 'darlene123', 'qwert', 0);
+insert into user values (39, 'Hannah', 'hannah123', 'qwert', 0);
+insert into user values (40, 'Gee', 'gee123', 'qwert', 0);
+insert into user values (41, 'Max', 'max123', 'qwert', 0);
+insert into user values (42, 'Eric', 'eric123', 'qwert', 0);
+insert into user values (43, 'Sumi', 'sumi123', 'qwert', 0);
+insert into user values (44, 'Iris', 'iris123', 'qwert', 0);
+insert into user values (45, 'Irene', 'irene123', 'qwert', 0);
+insert into user values (46, 'Madi', 'madi123', 'qwert', 0);
+insert into user values (47, 'Daniel', 'daniel123', 'qwert', 0);
+insert into user values (48, 'Caroline', 'caroline123', 'qwert', 1);
+insert into user values (49, 'Vince', 'vince123', 'qwert', 1);
+insert into user values (50, 'Judy', 'judy123', 'qwert', 1);
+insert into user values (51, 'Gina', 'gina123', 'qwerty', 1);
+insert into user values (52, 'Jason', 'jason123', 'qwerty', 1);
+insert into user values (53, 'Dareh', 'dareh123', 'qwerty', 1);
+insert into user values (54, 'Dana', 'dana123', 'qwerty', 1);
+insert into user values (55, 'Kate', 'kate123', 'qwerty', 1);
+insert into user values (56, 'Kait', 'kait123', 'qwerty', 1);
+insert into user values (57, 'Cait', 'Cait123', 'qwert', 1);
+insert into user values (58, 'Katelyn', 'katelyn123', 'qwerty', 1);
+insert into user values (59, 'Kaitlyn', 'kaitlyn123', 'qwerty', 1);
+insert into user values (60, 'Karen', 'karen123', 'qwerty', 1);
+insert into user values (61, 'Jimmy', 'jimmy123', 'qwerty', 1);
+insert into user values (62, 'Victor', 'victor123', 'qwerty', 1);
+insert into user values (63, 'Sharon', 'sharon123', 'qwerty', 1);
+insert into user values (64, 'Kent', 'kent123', 'qwerty', 1);
+insert into user values (65, 'Eddie', 'eddie123', 'qwerty', 1);
+insert into user values (66, 'Mimi', 'mimi123', 'qwert', 1);
+insert into user values (67, 'Aaron', 'aaron123', 'qwert', 1);
+insert into user values (68, 'Dude', 'dude123', 'qwert', 1);
+insert into user values (69, 'Hehe', 'hehe123', 'qwert', 1);
+insert into user values (70, 'Brent', 'brent123', 'qwert', 1);
+insert into user values (71, 'Shen', 'shen123', 'qwert', 1);
+insert into user values (72, 'Emily', 'emily123', 'qwert', 1);
+insert into user values (73, 'Dan', 'dan123', 'qwert', 1);
+insert into user values (74, 'Goog', 'goog123', 'qwert', 1);
+insert into user values (75, 'Grace', 'grace123', 'qwert', 1);
+insert into user values (76, 'Di', 'di123', 'qwert', 1);
+insert into user values (77, 'Vy', 'vy123', 'qwert', 1);
+insert into user values (78, 'Sean', 'sean123', 'qwert', 1);
+insert into user values (79, 'Anthony', 'anthony123', 'qwert', 1);
+insert into user values (80, 'Kyle', 'kyle123', 'qwert', 1);
+insert into user values (81, 'Vix', 'vix123', 'qwert', 1);
+insert into user values (82, 'Victoria', 'victoria123', 'qwert', 1);
+insert into user values (83, 'Jared', 'jared123', 'qwert', 1);
+insert into user values (84, 'Jay', 'jay123', 'qwert', 1);
+insert into user values (85, 'Shelly', 'shelly123', 'qwert', 1);
+insert into user values (86, 'Bo', 'bo123', 'qwert', 1);
+insert into user values (87, 'Colt', 'colt123', 'qwert', 1);
+insert into user values (88, 'Thi', 'thi123', 'qwert', 1);
+insert into user values (89, 'Chris', 'chris123', 'qwert', 1);
+insert into user values (90, 'Justin', 'justin123', 'qwert', 1);
+insert into user values (91, 'Brandon', 'brandon123', 'qwert', 1);
+insert into user values (92, 'Nan', 'nan123', 'qwert', 1);
+insert into user values (93, 'Owen', 'owen123', 'qwert', 1);
+insert into user values (94, 'Libby', 'libby123', 'qwert', 1);
+insert into user values (95, 'Erika', 'erika123', 'qwert', 2);
+insert into user values (96, 'Christian', 'christian123', 'qwert', 2);
+insert into user values (97, 'Jeremy', 'jeremy123', 'qwert', 2);
+insert into user values (98, 'Shannon', 'shannon123', 'qwert', 2);
+insert into user values (99, 'Jill', 'jill123', 'qwert', 2);
+insert into user values (100, 'Shan', 'shan123', 'qwert', 2);
+
+-- id, classname, userid
+insert into teacher values (201, 'Erikas Class', 95);
+insert into teacher values (202, 'Christians Class', 96);
+insert into teacher values (203, 'Jeremys Class', 97);
+insert into teacher values (204, 'Shannons Class', 98);
+insert into teacher values (205, 'Jills Class', 99);
+insert into teacher values (206, 'Shans Class', 100);
+
+
+-- id, pairid, parentid, teacherid, userid?
+insert into student values (101, 'sadk', 201, NULL, 1);
+insert into student values (102, 'sadk1', 201, NULL, 2);
+insert into student values (103, 'sadk2', 201, NULL, 3);
+insert into student values (104, 'sadk3', 201, NULL, 4);
+insert into student values (105, 'sadk4', 201, NULL, 5);
+insert into student values (106, 'sadk5', 201, NULL, 6);
+insert into student values (107, 'sadk6', 201, NULL, 7);
+insert into student values (108, 'sadk7', 202, NULL, 8);
+insert into student values (109, 'sadk8', 202, NULL, 9);
+insert into student values (110, 'sadk9', 202, NULL, 10);
+insert into student values (111, 'sadk11', 202, NULL, 11);
+insert into student values (112, 'sadk12', 202, NULL, 12);
+insert into student values (113, 'sadk10', 202, NULL, 13);
+insert into student values (114, 'sadk13', 202, NULL, 14);
+insert into student values (115, 'sadk14', 203, NULL, 15);
+insert into student values (116, 'sadk15', 203, NULL, 16);
+insert into student values (117, 'sadk16', 203, NULL, 17);
+insert into student values (118, 'sadk17', 203, NULL, 18);
+insert into student values (119, 'sadk18', 203, NULL, 19);
+insert into student values (120, 'sadk19', 203, NULL, 20);
+insert into student values (121, 'sadk20', 204, NULL, 21);
+insert into student values (122, 'sadk21', 204, NULL, 22);
+insert into student values (123, 'sadk22', 204, NULL, 23);
+insert into student values (124, 'sadk23', 204, NULL, 24);
+insert into student values (125, 'sadk24', 204, NULL, 25);
+insert into student values (126, 'sadk25', 204, NULL, 26);
+insert into student values (127, 'sadk26', 205, NULL, 27);
+insert into student values (128, 'sadk27', 205, NULL, 28);
+insert into student values (129, 'sadk28', 205, NULL, 29);
+insert into student values (130, 'sadk29', 205, NULL, 30);
+insert into student values (131, 'sadk30', 205, NULL, 31);
+insert into student values (132, 'sadk31', 205, NULL, 32);
+insert into student values (133, 'sadk32', 205, NULL, 33);
+insert into student values (134, 'sadk33', 206, NULL, 34);
+insert into student values (135, 'sadk34', 206, NULL, 35);
+insert into student values (136, 'sadk35', 206, NULL, 36);
+insert into student values (137, 'sadk36', 206, NULL, 37);
+insert into student values (138, 'sadk37', 206, NULL, 38);
+insert into student values (139, 'sadk38', 206, NULL, 39);
+insert into student values (140, 'sadk39', 206, NULL, 40);
+insert into student values (141, 'sadk40', 206, NULL, 41);
+insert into student values (142, 'sadk41', 202, NULL, 42);
+insert into student values (143, 'sadk42', 203, NULL, 43);
+insert into student values (144, 'sadk43', 204, NULL, 44);
+insert into student values (145, 'sadk44', 205, NULL, 45);
+insert into student values (146, 'sadk45', 206, NULL, 46);
+insert into student values (147, 'sadk46', 201, NULL, 47);
+
+-- id, studentid, userid
+insert into parent values (300, 1, 48);
+insert into parent values (301, 2, 49);
+insert into parent values (302, 3, 50);
+insert into parent values (303, 4, 51);
+insert into parent values (304, 5, 52);
+insert into parent values (305, 6, 53);
+insert into parent values (306, 7, 54);
+insert into parent values (307, 8, 55);
+insert into parent values (308, 9, 56);
+insert into parent values (309, 10, 57);
+insert into parent values (310, 11, 58);
+insert into parent values (311, 12, 59);
+insert into parent values (312, 13, 60);
+insert into parent values (313, 14, 61);
+insert into parent values (314, 15, 62);
+insert into parent values (315, 16, 63);
+insert into parent values (316, 17, 64);
+insert into parent values (317, 18, 65);
+insert into parent values (318, 19, 66);
+insert into parent values (319, 20, 67);
+insert into parent values (320, 21, 68);
+insert into parent values (321, 22, 69);
+insert into parent values (322, 23, 70);
+insert into parent values (323, 24, 71);
+insert into parent values (324, 25, 72);
+insert into parent values (325, 26, 73);
+insert into parent values (326, 27, 74);
+insert into parent values (327, 28, 75);
+insert into parent values (328, 29, 76);
+insert into parent values (329, 30, 77);
+insert into parent values (330, 31, 78);
+insert into parent values (331, 32, 79);
+insert into parent values (332, 33, 80);
+insert into parent values (333, 34, 81);
+insert into parent values (334, 35, 82);
+insert into parent values (335, 36, 83);
+insert into parent values (336, 37, 84);
+insert into parent values (337, 38, 85);
+insert into parent values (338, 39, 86);
+insert into parent values (339, 40, 87);
+insert into parent values (340, 41, 88);
+insert into parent values (341, 42, 89);
+insert into parent values (342, 43, 90);
+insert into parent values (343, 44, 91);
+insert into parent values (344, 45, 92);
+insert into parent values (345, 46, 93);
+insert into parent values (346, 47, 94);
+
+
+
+-- id, body, title, teacherid
+insert into resource values (400, 'https://www.youtube.com/watch?v=71h8MZshGSs', 'ABC Song', 201);
+insert into resource values (401, 'https://www.uticaschools.org/domain/482', 'General Resources', 202);
+insert into resource values (402, 'https://code.org/educate/curriculum/elementary-school', 'Learn to Code', 203);
+insert into resource values (403, 'https://www.timestables.com', 'Times Tables', 204);
+insert into resource values (404, 'http://www.themathpage.com/Arith/elementary.htm', 'Math Add/Subtract', 205);
+insert into resource values (405, 'https://www.science4us.com/free-resources/', 'Science Program', 206);
+
+-- id, body, title, parentid, teacherid
+insert into notification values (500, 'Remember to sign the field trip waiver!', 'Field Trip Reminder', 302, 201);
+insert into notification values (501, 'We have a science test tomorrow.', 'Test', 303, 202);
+insert into notification values (502, 'Your child was really good in class today!', 'Good Behavior', 304, 203);
+insert into notification values (503, 'Your child was misbehaving today during lunchtime.', 'Behavior Notice', 305, 204);
+insert into notification values (504, 'Homework due tomorrow.', 'Homework Reminder', 306, 205;
+insert into notification values (505, 'Early release tomorrow.', 'Early Release Notice', 307, 206);
+
+-- id, pdf_link, title, teacherid
+insert into document values (600, '100 Chart', 'https://www.homeschoolmath.net/worksheets/PDF/100_Chart_Empty.pdf', 201);
+insert into document values (601, 'Number Chart', 'https://www.homeschoolmath.net/worksheets/table-number-chart.php?form=1&min=6&blank_pc=75&cols=5&rows=10&step=10&colorstep=&font=sans-serif&FontSize=16pt&pad=18&ptitle=&Submit=Submit&PDF_worksheet=1', 202);
+insert into document values (602, 'Spelling Practice', 'http://www.englishbanana.com/worksheets/punctuation-marks-2/punctuation-marks-2-eg27.pdf', 203);
+insert into document values (603, 'Vocabulary Practice', 'http://www.englishbanana.com/worksheets/dictionary-practice-3/dictionary-practice-3-ev29.pdf', 204);
+insert into document values (604, 'Uranus Info', 'http://www.tlsbooks.com/uranusreadingcomp.pdf', 205);
+insert into document values (605, 'Light', 'http://www.tlsbooks.com/light.pdf', 206);
+
+-- id, score, subject, total_score, teacherid
+insert into grade values (701, 100, 0, 100, 201);
+insert into grade values (702, 100, 1, 100, 201);
+insert into grade values (703, 100, 2, 100, 201);
+insert into grade values (704, 100, 0, 100, 202);
+insert into grade values (705, 100, 1, 100, 202);
+insert into grade values (706, 100, 2, 100, 202);
+insert into grade values (707, 100, 0, 100, 203);
+insert into grade values (708, 100, 1, 100, 203);
+insert into grade values (709, 100, 2, 100, 203);
+insert into grade values (710, 100, 0, 100, 204);
+insert into grade values (711, 100, 1, 100, 204);
+insert into grade values (712, 100, 2, 100, 204);
+insert into grade values (713, 100, 0, 100, 205);
+insert into grade values (714, 100, 1, 100, 205);
+insert into grade values (715, 100, 2, 100, 205);
+insert into grade values (716, 100, 0, 100, 206);
+insert into grade values (717, 100, 1, 100, 206);
+insert into grade values (718, 100, 2, 100, 206);
+
+
+-- id, body, title, teacherid
+insert into announcement values (800, 'Picture day is tomorrow!', 'Picture Day!', 201);
+insert into announcement values (801, 'We have a new student teacher joining us!', 'New Face in the Classroom!', 202);
+insert into announcement values (802, 'Show and tell is tomorrow, make sure your student has something to show!', 'Show and Tell', 203);
+insert into announcement values (803, 'No chewing gum in class!', 'Gum Warning!', 204);
+insert into announcement values (804, 'All students need a packed lunch tomorrow!', 'Field Trip Reminder!', 205);
+insert into announcement values (805, 'Parent Teacher conferences are coming up. Please contact me to schedule an appointment!', 'Parent Teacher Conference', 206);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
