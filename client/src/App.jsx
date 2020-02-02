7c5a54590aa4c91d2ef6c14b4dd2760da7a40068
Victor Tran <vt.victortran@gmail.com>
2020-02-01 19:20:51 -0800

    landing page is like almost done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import Landing from "./components/Landing";
 import { makeStyles } from "@material-ui/core";
 import Student from "./components/student/Student";
 import Announcements from "./components/student/Announcements";
+import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
+import { createMuiTheme } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
-    height: "100%",
+    height: "100%"
   }
 }));
 
@@ -17,16 +19,29 @@ const App = () => {
   const classes = useStyles();
 
   return (
-    <BrowserRouter>
-      <div className={classes.root}>
-        <Switch>
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/student" component={Student} />
-          <Route exact path="/announcement" component={Announcements} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <div>
+      <MuiThemeProvider theme={THEME}></MuiThemeProvider>
+      <BrowserRouter>
+        <div className={classes.root}>
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/student" component={Student} />
+            <Route exact path="/announcement" component={Announcements} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
   );
 };
+
+const THEME = createMuiTheme({
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+    fontSize: 14,
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500
+  }
+});
 
 export default App;
--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -1,14 +1,81 @@
-import React from "react";
+import React, { Component, Fragment } from "react";
 
-import { Grid, Typography, Button, makeStyles } from "@material-ui/core";
+import {
+  Grid,
+  Typography,
+  Button,
+  makeStyles,
+  CssBaseline
+} from "@material-ui/core";
+import Component1 from "./FadeIn";
+import Component2 from "./DropDown";
+import ReactDOM from "react-dom";
+import { shadows } from "@material-ui/system";
+import { withStyles } from "@material-ui/core/styles";
+
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Paper from "@material-ui/core/Paper";
+import Icon from "@material-ui/core/Icon";
+import StudentLogo from "../svg/001-superhero.svg";
+import TeacherLogo from "../svg/002-superhero.svg";
+import ParentLogo from "../svg/003-superhero.svg";
+import MainLogo from "../svg/047-superhero.svg";
+import AboutLogo from "../svg/016-superhero.svg";
+import CompanyLogo from "../svg/flasks.svg";
+
+import "./styles.css";
 
 const useStyles = makeStyles(theme => ({
   container: {
     maxHeight: "100%",
     maxWidth: "100%"
   },
+  itemLeft: {
+    backgroundColor: "blue"
+  },
+  itemCenter: {
+    justifyContent: "center"
+  },
+  itemRight: {
+    backgroundColor: "red"
+  },
   landingButtons: {
     padding: "1em 2em"
+  },
+  card: {
+    minWidth: 200,
+    minHeight: 150,
+    maxWidth: 300,
+    margin: "auto",
+    transition: "0.3s",
+    boxShadow: "0 4px 20px -12px rgba(0,0,0,0.3)",
+    "&:hover": {
+      boxShadow: "0 16px 70px -12.125px rgba(0,0,0,0.3)"
+    }
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  },
+  heroMain: {
+    paddingTop: "5em"
+  },
+  aboutHero: {
+    paddingTop: "5em"
+  },
+  companyLogo: {
+    paddingTop: ".3em",
+    paddingRight: ".3em"
+  },
+  title: {
+    flexDirection: "row"
+  },
+  signUp: {
+    paddingTop: "1.5em"
   }
 }));
 
@@ -24,31 +91,131 @@ const Landing = () => {
       spacing={3}
       className={classes.container}
     >
-      <Grid item>
-        <Typography variant="h1">Procuratio</Typography>
-      </Grid>
-      <Grid item container justify="center">
-        <Grid item className={classes.itemRight}>
-          <Button
-            variant="contained"
-            color="primary"
-            disableElevation
-            className={classes.landingButtons}
-          >
-            <Typography variant="h4">Login</Typography>
-          </Button>
+      {/*<Grid
+        item
+        container
+        className={classes.itemCenter}
+        justify="center"
+        justifyContent="center"
+      >
+        <Component1 />
+      </Grid>*/}
+      <section id="main">
+        <div className="main-text">
+          <h1 className={classes.title}>
+            <img
+              className={classes.companyLogo}
+              src={CompanyLogo}
+              width="80em"
+            />
+            Hero Labs
+          </h1>
+          <p className="main-desc">
+            Where intuitive classroom management develops the next generation of
+            heroes. <br /> <br /> <br /> <br /> Sign up as a...
+          </p>
+        </div>
+        <img className={classes.heroMain} src={MainLogo} width="420" />
+      </section>
+
+      <Grid
+        item
+        container
+        className={classes.itemCenter}
+        direction="row"
+        justify="center"
+        alignItems="center"
+      >
+        <Grid item container className={classes.itemCenter} justify="center" xs>
+          {/*card actions are a thing btw*/}
+          <Card className={classes.card} variant="outlined">
+            <CardContent>
+              <Typography
+                fontWeight="200"
+                variant="h5"
+                component="h2"
+                align="center"
+              >
+                Parent
+              </Typography>
+              <Grid
+                item
+                container
+                className={classes.itemCenter}
+                justify="center"
+                xs
+              >
+                <img src={ParentLogo} height={100} width={100} />
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
-        <Grid item xs={1} />
-        <Grid item className={classes.itemLeft}>
-          <Button
-            variant="contained"
-            color="primary"
-            disableElevation
-            className={classes.landingButtons}
-          >
-            <Typography variant="h4">Teacher</Typography>
-          </Button>
+
+        <Grid
+          item
+          container
+          className={classes.itemCenter}
+          justify="center"
+          alignItems="center"
+          xs
+        >
+          <Card className={classes.card} variant="outlined">
+            <CardContent justify="center">
+              <Typography variant="h5" component="h2" align="center">
+                Student
+              </Typography>
+              <Grid
+                item
+                container
+                className={classes.itemCenter}
+                justify="center"
+                xs
+              >
+                <img src={StudentLogo} height={100} width={100} />
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
+
+        <Grid item container className={classes.itemCenter} justify="center" xs>
+          <Card className={classes.card} variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="h2" align="center">
+                Teacher
+              </Typography>
+              <Grid
+                item
+                container
+                className={classes.itemCenter}
+                justify="center"
+                xs
+              >
+                <img src={TeacherLogo} height={100} width={100} />
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+
+        <section id="about">
+          <div className="about-text">
+            <h1>It just got a whole lot easier to manage a classroom.</h1>
+            <p className="about-desc">
+              <strong>An all-new classroom management system.</strong> <br /> We
+              connect elementary school teachers with students and parents to
+              build amazing communities.
+            </p>
+          </div>
+          <img
+            className={classes.aboutHero}
+            src={AboutLogo}
+            width="420"
+            alt="hero-main-image"
+          />
+        </section>
+
+        <footer>
+          <p className="footer">Designed & Built by the Frontend Gang 🤪</p>
+        </footer>
       </Grid>
     </Grid>
   );
